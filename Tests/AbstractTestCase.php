1706dd2e0a66100ba429b584d1e19ce035bdb437
webeweb <support@webeweb.fr>
2019-05-10 17:41:04 +0200

    Add abstract test case

---
+++ Tests/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the haveibeenpwned-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\HaveIBeenPwnedBundle\Tests;
+
+use WBW\Bundle\CoreBundle\Tests\AbstractTestCase as TestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\HaveIBeenPwnedBundle\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends TestCase {
+
+}
